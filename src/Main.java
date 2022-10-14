b1909898360acf1976e56c4e52c737583b4ac2a1
Nguyễn Xuân Tuân <tuannguyenxuancv@gmail.com>
2022-10-14 15:38:04 +0700

    class student

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+        Student student = new Student();
+
+        student.setAge(22);
+        student.setName("Tuân");
+        System.out.println("Tên student: " + student.getName() + " tuổi là : "+student.getAge());
+        Student student1 = new Student(10,"Vinh");
+        student1.display();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    int age;
+    String name;
+
+    Student (){
+
+    }
+
+    Student (int age, String name){
+       this.age = age;
+       this.name = name;
+    }
+
+    void display() {
+        System.out.println("Tên học sinh là: "+ name + " tuổi là: "+age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
